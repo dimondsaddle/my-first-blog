a2482a47dd6633b1b500a48060dcd90d43fdcf23
dimon <benjaminashpole@gmail.com>
2021-11-04 13:21:59 +0000

    sadge

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
 ]
 LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
